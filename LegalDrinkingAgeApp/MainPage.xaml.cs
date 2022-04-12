8a0ea3a5f6a22b02a5717fe727a3aad6eed175f5
Tyler Bach <Tbach2@my.wctc.edu>
2022-04-11 22:37:18 -0700

    Add project files.

---
+++ LegalDrinkingAgeApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LegalDrinkingAgeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        int CurrentYear = DateTime.Today.Year;
+        int BirthYear;
+        int Age;
+        int LegalAgeUS = 21; 
+
+        void Handle_Clicked(Object sender, System.EventArgs e)
+        {
+            BirthYear = Birthday.Date.Year;
+            Age = CurrentYear - BirthYear;
+
+            if(Age >= LegalAgeUS)
+            {
+                Text.Text = $"Right now!";
+            }
+            else
+            {
+                Text.Text = $"You will be able to drink legally in {LegalAgeUS - Age} years!";
+            }
+        }
+    }
+}
